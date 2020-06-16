300a1b70c5c357fdac936483690df4ae472a5ad9
amarkham1 <andrewjmarkham1@gmail.com>
2020-06-15 21:40:03 -0400

    adding hashtable

---
+++ hashTable.js
@@ -0,0 +1,55 @@
+class HashTable {
+  constructor(size){
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i =0; i < key.length; i++){
+        hash = (hash + key.charCodeAt(i) * i) % this.data.length
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key){
+    const address = this._hash(key);
+    const currentBucket = this.data[address]
+    if (currentBucket) {
+      for(let i = 0; i < currentBucket.length; i++){
+        if(currentBucket[i][0] === key) {
+          return currentBucket[i][1]
+        }
+      }
+    }
+    return undefined;
+  }
+
+  keys() {
+    if (!this.data.length) { 
+      return undefined;
+    }
+
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        if (this.data.length[i] > 1) {
+          for (let j = 0; j < this.data[j].length; j++) {
+            keysArray.push(this.data[i][j][0]);
+          }
+        } else {
+          keysArray.push(this.data[i][0]);
+        }
+      }
+    }
+    return keysArray;
+  }
+}
