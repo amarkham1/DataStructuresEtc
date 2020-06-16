ebdd5e65a972c9dd7f5fda11ab710f83d1f5f3e1
amarkham1 <andrewjmarkham1@gmail.com>
2020-06-15 21:03:32 -0400

    added array.js

---
+++ array.js
@@ -0,0 +1,56 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(data) {
+    this.data[this.length] = data;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    if (index > this.length - 1 || index < 0) {
+      throw new TypeError('please input a valid index to delete');
+    }
+    
+    const item = this.data[index];    
+    for (let i = index; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    return item;
+  }
+
+  shift() {
+    let shiftedValue = this.data[0];
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+    return shiftedValue;
+  }
+
+  unshift(data) {
+    if (data) {
+      for (let i = this.length; i >= 1; i--) {
+        this.data[i] = this.data[i - 1];
+      }
+      this.data[0] = data;
+      return this.data[0];
+    }
+  }
+}
